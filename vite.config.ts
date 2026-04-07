cd026b098120b5ec0764e8fbfcd26778a903ac14
Administrator <admin@example.com>
2026-04-07 20:18:47 +0800

    Update vite.config.ts for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Devive-Manager/',
   build: {
     minify: true,
     sourcemap: false,
